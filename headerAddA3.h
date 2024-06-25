5aae3ccca43dee55b29db2fd451ad9fd5b9804cf
Jess <JessicaNguyenz09@gmail.com>
2024-06-25 17:28:29 -0400

    Update headerAddA3.h

--- headerAddA3.h
+++ headerAddA3.h
@@ -1,18 +1,6 @@
 /* 
-  Student Name: Jessica Nguyen
-  Student ID: 1169812
-  Due Date: March 31, 2022 
-  Course: CIS*2500 
- 
-  I have exclusive control over this submission via my password. 
-  By including this header comment, I certify that: 
-   1) I have read and understood the policy on academic integrity. 
-   2) I have completed Moodle's module on academic integrity. 
-   3) I have achieved at least 80% on the academic integrity quiz 
-  I assert that this work is my own. I have appropriate acknowledged 
-  any and all material that I have used, be it directly quoted or 
-  paraphrased. Furthermore, I certify that this assignment was written 
-  by me in its entirety. 
+  Creator: Jessica Nguyen
+  Due Date: 2022-03-31 
 */
 
 #include <time.h> // Allows for random seed
